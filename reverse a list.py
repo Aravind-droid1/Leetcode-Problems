7d99fb065e4e32299bf0955622c7ed9a0b0ea825
Aravind P <aravindshankar486@gmail.com>
2025-02-14 22:52:48 +0530

    Add files via upload

---
+++ reverse a list.py
@@ -0,0 +1,13 @@
+#2235 add two integers
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        return num1+num2
+    
+obj=Solution()
+print(obj.sum(3,-4))
+print(obj.sum(4,6))
